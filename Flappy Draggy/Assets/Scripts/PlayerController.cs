7ae7d0ca7338f497e1f0a2ae461f31adaa32f732
Jonathon Hamrick <jonathon.hamrick@gmail.com>
2021-10-05 10:53:40 -0400

    Top and Bottom Boundaries added.. Draggy movement added..

--- Flappy Draggy/Assets/Scripts/PlayerController.cs
+++ Flappy Draggy/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // variables
+    [SerializeField] private float UpSpeed;
+
+    // components
+    private Animator anim;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetButtonDown ("Fire1")) 
+		// check for user input
+        if (Input.GetButtonDown ("Fire1")) 
 		{
-			GetComponent<Animator> ().SetTrigger ("Fly");
+            // trigger animation
+			anim.SetTrigger ("Fly");
+
+            // move up
+            Vector2 dir;
+            dir = Vector2.zero;
+            dir.y = 1;
+
+            rb.velocity += dir * UpSpeed;
 		}
+    }
+
+    void FixedUpdate()
+    {
+        
     }
 }
